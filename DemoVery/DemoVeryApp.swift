ff129ec09a54b3ef4f91ff29d4a4a7b4053330b7
Veronica <veronica.licea_moreno@yahoo.com>
2022-06-20 06:27:55 -0700

    DemoVery

---
+++ DemoVery/DemoVeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoVeryApp.swift
+//  DemoVery
+//
+//  Created by Apprenant 16 on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoVeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
